ab6f12f3205ac5f2f4661b137fbd3868684cb712
Ryan Andri <ryan.omnia@gmail.com>
2016-01-03 20:23:58 +0700

    s2w/dt2w: make default to lcd notify.

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -29,11 +29,7 @@
 #include <linux/slab.h>
 #include <linux/workqueue.h>
 #include <linux/input.h>
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#else
 #include <linux/lcd_notify.h>
-#endif
 #include <linux/hrtimer.h>
 #include <asm-generic/cputime.h>
 
@@ -73,9 +69,7 @@ static cputime64_t tap_time_pre = 0;
 static int touch_x = 0, touch_y = 0, touch_nr = 0, x_pre = 0, y_pre = 0;
 static bool touch_x_called = false, touch_y_called = false, touch_cnt = true;
 static bool scr_suspended = false, exec_count = true;
-#ifndef CONFIG_POWERSUSPEND
 static struct notifier_block dt2w_lcd_notif;
-#endif
 static struct input_dev * doubletap2wake_pwrdev;
 static DEFINE_MUTEX(pwrkeyworklock);
 static struct workqueue_struct *dt2w_input_wq;
@@ -284,22 +278,8 @@ static struct input_handler dt2w_input_handler = {
 	.id_table	= dt2w_ids,
 };
 
-#ifdef CONFIG_POWERSUSPEND
-static void dt2w_power_suspend(struct power_suspend *h) {
-	scr_suspended = true;
-}
-
-static void dt2w_power_resume(struct power_suspend *h) {
-	scr_suspended = false;
-}
-
-static struct power_suspend dt2w_power_suspend_handler = {
-	.suspend = dt2w_power_suspend,
-	.resume = dt2w_power_resume,
-};
-#else
 static int lcd_notifier_callback(struct notifier_block *this,
-								unsigned long event, void *data)
+				unsigned long event, void *data)
 {
 	switch (event) {
 	case LCD_EVENT_ON_END:
@@ -314,7 +294,6 @@ static int lcd_notifier_callback(struct notifier_block *this,
 
 	return 0;
 }
-#endif
 
 /*
  * SYSFS stuff below here
@@ -425,14 +404,10 @@ static int __init doubletap2wake_init(void)
 	if (rc)
 		pr_err("%s: Failed to register dt2w_input_handler\n", __func__);
 
-#ifdef CONFIG_POWERSUSPEND
-	register_power_suspend(&dt2w_power_suspend_handler);
-#else
 	dt2w_lcd_notif.notifier_call = lcd_notifier_callback;
 	if (lcd_register_client(&dt2w_lcd_notif) != 0) {
 		pr_err("%s: Failed to register lcd callback\n", __func__);
 	}
-#endif
 
 #ifndef ANDROID_TOUCH_DECLARED
 	android_touch_kobj = kobject_create_and_add("android_touch", NULL) ;
@@ -468,9 +443,7 @@ static void __exit doubletap2wake_exit(void)
 #ifndef ANDROID_TOUCH_DECLARED
 	kobject_del(android_touch_kobj);
 #endif
-#ifndef CONFIG_POWERSUSPEND
 	lcd_unregister_client(&dt2w_lcd_notif);
-#endif
 	input_unregister_handler(&dt2w_input_handler);
 	destroy_workqueue(dt2w_input_wq);
 	input_unregister_device(doubletap2wake_pwrdev);
