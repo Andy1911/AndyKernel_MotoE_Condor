8d3877c21c98f3c35312406115887f4c5e8b0032
ryan-andri <ryanandri@hotmail.com>
2016-11-30 16:40:52 +0700

    build helper : remove obsolote

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 rm -f /home/ryanandri07/android/android_kernel_motoe/clarity-N-condor/boot/zImage-dtb
 rm -f /home/ryanandri07/android/android_kernel_motoe/clarity-N-condor/system/lib/modules/*.ko
-rm -f /home/ryanandri07/android/android_kernel_motoe/clarity-N-condor/system/lib/modules/pronto/pronto_wlan.ko
 rm -f /home/ryanandri07/android/android_kernel_motoe/arch/arm/boot/zImage
 
 mv /home/ryanandri07/android/android_kernel_motoe/arch/arm/boot/zImage-dtb /home/ryanandri07/android/android_kernel_motoe/clarity-N-condor/boot
@@ -14,10 +13,8 @@ sleep 2
 # Remove Unneeded
 /home/ryanandri07/android/linaro-cortex-a7-4.9/bin/arm-cortex_a7-linux-gnueabihf-strip --strip-unneeded /home/ryanandri07/android/android_kernel_motoe/clarity-N-condor/system/lib/modules/*.ko
 
-# move to proper place
-mv /home/ryanandri07/android/android_kernel_motoe/clarity-N-condor/system/lib/modules/wlan.ko /home/ryanandri07/android/android_kernel_motoe/clarity-N-condor/system/lib/modules/pronto/pronto_wlan.ko
-
-DATE = `date +%d-%m-%Y`;
 cd /home/ryanandri07/android/android_kernel_motoe/clarity-N-condor
+
+DATE=`date +%d-%m-%Y`;
 zip -r /home/ryanandri07/android/release/Clarity-N-$DATE.zip *
 
